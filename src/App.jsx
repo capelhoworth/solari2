1659f2f6c1a8e9feff018268e94f351149855c28
Capel Howorth <capelhoworth@gmail.com>
2024-05-12 10:44:11 -0400

    dat file loaded and axios working

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react"
+import axios from 'axios';
+//import ReportingAreaConversion from "./components/ReportingAreaConversion"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  console.log('app render');
+  const [reportingData, setReportingData] = useState([]);
+  //console.log(ReportingAreaConversion)
+
+  const fetchData = async () => {
+    console.log('fetchData');
+      try {
+          const response = await axios.get('public/reportingarea.dat');
+          const data = response.data;
+
+          // Split data by newline and then by pipe (|)
+          const parsedData = data.split('\n').map(line => line.split('|'));
+
+          // Convert parsed data into JSON format
+          const jsonData = parsedData.map(line => ({
+              startDate: line[0],
+              endDate: line[1],
+              startTime: line[2] || '',
+              timeZone: line[3],
+          }));
+          console.log('!');
+            console.log(jsonData);
+          // // state with JSON data
+          //setReportingData(jsonData);
+      } catch (error) {
+          console.error('Error fetching data:', error);
+      }
+     // console.log(ReportingAreaConversion)
+  };
+
+  useEffect(() => {
+
+    console.log(1);
+    fetchData();
+  []});
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Howdy! Thee</h1>
+      <h2>Reporting Area Data</h2>
+      <pre>{JSON.stringify(reportingData, null, 2)}</pre>
+      {/* <ReportingAreaConversion /> */}
     </>
   )
 }
 
 export default App
+
