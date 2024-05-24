bfc49bf3b77a0d37e471aff1a3bd8bb4fcc80d80
Capel Howorth <capelhoworth@gmail.com>
2024-05-24 10:17:23 -0400

    changes spans to divs in wrapCharacters

--- src/components/TopTen.jsx
+++ src/components/TopTen.jsx
@@ -16,32 +16,32 @@ function TopTen({data}) {
     // Get top 10 aqiValues
     const topTenData = sortedData.slice(0, 10);
 
-    useEffect(() => {
-        const flippers = document.querySelectorAll(".flipper");
-        const handleAnimationEnd = (event) => {
-                event.target.classList.add("animation-ended");
-            };
-            flippers.forEach((flipper) => {
-                flipper.addEventListener("animationend", handleAnimationEnd);
-        });
-        return () => {
-            flippers.forEach((flipper) => {
-                flipper.removeEventListener("animationend", handleAnimationEnd);
-            });
-        };
-    }, []);
+    // useEffect(() => {
+    //     const flippers = document.querySelectorAll(".flipper");
+    //     const handleAnimationEnd = (event) => {
+    //             event.target.classList.add("animation-ended");
+    //         };
+    //         flippers.forEach((flipper) => {
+    //             flipper.addEventListener("animationend", handleAnimationEnd);
+    //     });
+    //     return () => {
+    //         flippers.forEach((flipper) => {
+    //             flipper.removeEventListener("animationend", handleAnimationEnd);
+    //         });
+    //     };
+    // }, []);
 
     // logic/mapping splits string into characters and wraps each with flip animation structure
     const wrapCharacters = (str, rowIndex) => (
         str.split('').map((char, index) => (
-            <span className="tickerWrap animating" key={`wrapper-${index}`} style={{ animationDelay: `${rowIndex * 0.5}s` }}>
-                <span className="cover">
-                    <span className="flipper">
-                        <span className="tickerTxt">{char}</span>
-                        {/* <span className="tickerTxt inverse hidden-after-animation">{getRandomChar(char)}</span> */}
-                    </span>
-                </span>
-            </span>
+            <div className="tickerWrap animating" key={`wrapper-${index}`} style={{ animationDelay: `${rowIndex * 0.5}s` }}>
+                <div className="cover">
+                    <div className="flipper">
+                        <div className="tickerTxt">{char}</div>
+                        {/* <div className="tickerTxt inverse hidden-after-animation">{getRandomChar(char)}</div> */}
+                    </div>
+                </div>
+            </div>
         ))
     );
 
